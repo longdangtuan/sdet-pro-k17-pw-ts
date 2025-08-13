c8fb7bca309d0e28a332801eb9634f0f431a8b2d
longdangtuan <longdangtuan0706@gmail.com>
2025-08-13 23:55:04 +0700

    feat: lab 5

---
+++ lab/lab5/lab5.js
@@ -0,0 +1,18 @@
+const readline = require("readline-sync");
+const givenString = String(readline.question("Please enter your desire text:"));
+
+newString = givenString.replace(/[,.]/g, " ");
+
+const splitWords = newString.split(" ");
+const nonEmptyArray = splitWords.filter(function (word) {
+  return word.trim() !== "";
+});
+console.log(nonEmptyArray);
+
+const countedWords = {};
+for (const word of nonEmptyArray) {
+  const currCount = countedWords[word] ?? 0;
+  countedWords[word] = currCount + 1;
+}
+
+console.log(countedWords);
